f71b9dd4a202e81706a5d7f148ef6b77e2d6c8a5
David Hachuel <dhachuel@gmail.com>
2017-09-14 20:14:59 -0400

    Fixed @ decorator Flask

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.route('/')
+@app.route("/")
 def hello():
-    return("Hello FaceCrederrrrszzz!!!")
+    return("Hello FaceCrederrrrszzz")
+
+
+if __name__ == "__main__":
+    app.run()
